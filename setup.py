718ec84cf204bf21e3c794128f2cc04ef92ab3a2
M. Huang <mhuang@nao.cas.cn>
2019-04-16 13:50:26 +0800

    initial checkin

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+#
+#
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="dataset",
+    version="0.1",
+    author="Maohai Huang",
+    author_email="mhuang@earth.bao.ac.cn",
+    description=("packaging data into self-describing Data Products"),
+    license="GPL",
+    keywords="dataset product metadata HCSS",
+    url="packaging data into self-describing Data Products",
+    packages=['dataset', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPL License",
+    ],
+)

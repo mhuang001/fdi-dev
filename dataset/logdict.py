09e572191d7e18beeb63861de5b1e72d92dfb88f
M. Huang <mhuang@nao.cas.cn>
2019-04-16 12:14:38 +0800

    log file name change

--- dataset/logdict.py
+++ dataset/logdict.py
@@ -21,7 +21,7 @@ logdict = {
             "class": "logging.handlers.RotatingFileHandler",
             "level": "DEBUG",
             "formatter": "full",
-            "filename": "pns.log",
+            "filename": "dataset.log",
             "maxBytes": 20000000,
             "backupCount": 3
         }

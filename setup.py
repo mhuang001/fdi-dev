495a555d6884004e193f395011b3f04b31fad6e3
M. Huang <mhuang@nao.cas.cn>
2021-12-14 06:22:17 +0800

    http for nao gitlab mirror

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'filelock>=3.0.12',
         'ruamel.yaml<0.17',
         #'tabulate @ git+https://github.com/mhuang001/python-tabulate.git@r1',
-        'tabulate @ git+https://mercury.bao.ac.cn:9006/mirrors/tabulate.git@r1',
+        'tabulate @ git+http://mercury.bao.ac.cn:9006/mirrors/tabulate.git@r1',
         'cwcwidth>=0.1.5',
         'paho-mqtt>=1.5.1',
         'xmltodict>=0.12.0',

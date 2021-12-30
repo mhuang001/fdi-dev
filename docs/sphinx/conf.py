274d06879768a0dbb2d2ddb0d0c1ae885b7e90ea
JL <jia@x.local>
2021-12-30 09:57:50 +0800

    index

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -22,6 +22,9 @@ sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
+language = 'zh_CN' # language supported
+locale_dirs = ['locale/'] # path is example but recommended.
+gettext_compact = False # optional.
 
 project = 'fdi'
 year = datetime.now().year
